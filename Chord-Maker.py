6d73e2f9fd916495e48d6581d78acf9f3c56a175
Jimzical <arishkumar2003@gmail.com>
2024-03-09 23:19:23 +0530

    renamed file to make more sense and added docs

--- Chord-Maker.py
+++ Chord-Maker.py
@@ -1,9 +1,41 @@
+'''
+Chord-Maker.py
+
+This is a simple program to test out the sound files in the samples folder.
+They have the sound of a guitar string being plucked.
+
+# Aim:
+The Goal is to make a program that can play the sound of a guitar chords by choosing the right combination of sound files.
+
+# Current State:
+The program can play the sound of the guitar strings by clicking the buttons.
+The program can also play a song using the chords made from the sound files.
+Chords present are
+    - Em
+    - D
+    - G
+    - C
+'''
+
+
+
 import PySimpleGUI as sg
 import os
 from pygame import mixer
 from time import sleep
+from pygame import mixer
 
-def GUI_Builder():
+
+def GUI_Builder() -> sg.Window:
+    '''
+    Description:
+    ------------
+    This function builds the GUI for the sound player application.
+
+    Returns:
+    --------
+    sg.Window: The window object representing the GUI.
+    '''
     sg.theme('DarkBrown2')
 
     layout = []
@@ -25,26 +57,59 @@ def GUI_Builder():
     wind = sg.Window('Sound Player', layout= layout, grab_anywhere=True)
     return wind
 
+# Mixer is used in this functions as it allows multiple sounds to be played at the same time.
+def stringDict() -> dict:
+    '''
+    Description:
+    ------------
+    This function returns a dictionary with the string notes mapped to the sound files as values.
+    The keys are the string notes and the values are the sound files.
 
-def stringDict():
+    Returns:
+    --------
+    dict: A dictionary with the string notes as keys and the sound files as values.
+    '''
     # d = {0 : 'E4.wav', 1 : 'B3.wav', 2 : 'G3.wav', 3 : 'D3.wav', 4 : 'A2.wav', 5 : 'E2.wav'}
     # d = {'e' : 'E4.wav', 'B' : 'B3.wav', 'G' : 'G3.wav', 'D' : 'D3.wav', 'A' : 'A2.wav', 'E' : 'E2.wav'}
     d = {'E' : 'E2.wav', 'A' : 'A2.wav', 'D' : 'D3.wav', 'G' : 'G3.wav', 'B' : 'B3.wav', 'e' : 'E4.wav' }
     return d
 
+def player(d) -> None:
+    """
+    Description:
+    ------------
+    This function plays the sound files mapped in the dictionary d.
 
-def player(d):
+    Parameters:
+    -----------
+    d : dict
+        A dictionary with the string notes as keys and the sound files as values.
+
+    Returns:
+    --------
+    None
+    """
     index = 0
     for key in d:
         mixer.Channel(index).play(mixer.Sound('samples/' + d[key]))
         index += 1
 def Em():
+    '''
+    Description:
+    ------------
+    This function plays the sound of the Em chord.
+    '''
     d = stringDict()
     d['A'] = "B2.wav"
     d['D'] = "E3.wav"
     player(d)
 
 def D():
+    '''
+    Description:
+    ------------
+    This function plays the sound of the D chord.
+    '''
     d = stringDict()
     # removeing stringe
     e = d.pop('E')
@@ -59,6 +124,11 @@ def D():
     #     index += 1
 
 def G():
+    '''
+    Description:
+    ------------
+    This function plays the sound of the G chord.
+    '''
     d = stringDict()
     d["E"] = "G2.wav"
     d["e"] = "G4.wav"
@@ -66,6 +136,11 @@ def G():
     player(d)
 
 def C():
+    '''
+    Description:
+    ------------
+    This function plays the sound of the C chord.
+    '''
     d = stringDict()
     d.pop('E')
     d["A"] = "C3.wav"
@@ -81,16 +156,22 @@ def main():
     print(samplelist)
 
     while True:
-        event, values = window.read()
+        event, _ = window.read()
         print(event)
+
+        # if the window is closed, break the loop
         if event == sg.WIN_CLOSED:
             break
+        
+        # play the sound of the button clicked
         if event in samplelist:
             print('Playing sound', event)
             # give varible e2 the sound
             sound = mixer.Sound('samples/' + event + '.wav')
             sound = mixer.Channel(0).play(sound)
             # sound.play()
+
+        # play a song using chords made from the sound files
         if event == 'Play Sound':
             print('Playing sound')
             loop = 0
